fe97b567164c047ac5d63d0c16d8e0539f049afd
codeZeus-Ojonaw <lewechigodsfavour@gmail.com>
2025-03-23 05:37:18 +0100

    feat: font, asset and text system

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:zkauth/core/routers/routers.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -30,7 +31,7 @@ class _AppState extends State<App> {
       splitScreenMode: false,
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
-        title: 'Streamfi',
+        title: 'ZKAuth',
         scrollBehavior: const _AppScrollBehavior(),
         theme: ThemeData(
           primarySwatch: Colors.grey,
---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,12 @@
+class AppAssets {
+  AppAssets._();
+
+  static const zkauthIconPath = 'assets/icons/';
+  //! START
+  static String userIcon = '${zkauthIconPath}user.svg';
+
+  //-------------------------------------------- IMAGES -------------------------------------------- //
+  static const zkauthImagePath = 'assets/images';
+  //! START
+  static String people = '${zkauthImagePath}people.png';
+}
